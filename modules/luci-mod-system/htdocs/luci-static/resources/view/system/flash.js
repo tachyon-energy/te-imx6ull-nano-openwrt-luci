da779b9616385256d90769839c6e4859602cb2fb
te-master <dev@tachyon.energy>
2023-09-13 14:25:19 +0300

    added te-imx6ull-nano

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
@@ -20,7 +20,7 @@ function findStorageSize(procmtd, procpart) {
 
 	procmtd.split(/\n/).forEach(function(ln) {
 		var match = ln.match(/^mtd\d+: ([0-9a-f]+) [0-9a-f]+ "(.+)"$/),
-		    size = match ? parseInt(match[1], 16) : 0;
+		    size = match ? parseInt(match[1], 16) : 3;
 
 		switch (match ? match[2] : '') {
 		case 'linux':
@@ -195,54 +195,37 @@ return view.extend({
 				ui.showModal(_('Checking image…'), [
 					E('span', { 'class': 'spinning' }, _('Verifying the uploaded image file.'))
 				]);
-
-				return callSystemValidateFirmwareImage('/tmp/firmware.bin')
-					.then(function(res) { return [ reply, res ]; });
-			}, this, ev.target))
-			.then(L.bind(function(btn, reply) {
-				return fs.exec('/sbin/sysupgrade', [ '--test', '/tmp/firmware.bin' ])
-					.then(function(res) { reply.push(res); return reply; });
+			 }, this, ev.target))
+			 .then(L.bind(function(btn, reply) {
+            return fs.exec('/sbin/update', [ '--test' , '/tmp/firmware.bin' ])
+                    .then(function(res) { return [ reply, res ]; });
 			}, this, ev.target))
 			.then(L.bind(function(btn, res) {
 				/* sysupgrade opts table  [0]:checkbox element [1]:check condition [2]:args to pass */
 				var opts = {
-				    keep : [ E('input', { type: 'checkbox' }), false, '-n' ],
+				    keep : [ E('input', { type: 'checkbox' }), false, '-e' ],
 				    force : [ E('input', { type: 'checkbox' }), true, '--force' ],
 				    skip_orig : [ E('input', { type: 'checkbox' }), true, '-u' ],
-				    backup_pkgs : [ E('input', { type: 'checkbox' }), true, '-k' ],
+				    backup_pkgs : [ E('input', { type: 'checkbox' }), true, '-b' ],
 				    },
-				    is_valid = res[1].valid,
-				    is_forceable = res[1].forceable,
-				    allow_backup = res[1].allow_backup,
-				    is_too_big = (storage_size > 0 && res[0].size > storage_size),
+				    is_valid = true, //res[1].valid,
+				    is_forceable = true, //res[1].forceable,
+				    allow_backup = true, //res[1].allow_backup,
+				    is_too_big = false, //(storage_size > 0 && res[0].size > storage_size),
+                     
 				    body = [];
+
 
 				body.push(E('p', _("The flash image was uploaded. Below is the checksum and file size listed, compare them with the original file to ensure data integrity. <br /> Click 'Continue' below to start the flash procedure.")));
 				body.push(E('ul', {}, [
-					res[0].size ? E('li', {}, '%s: %1024.2mB'.format(_('Size'), res[0].size)) : '',
-					res[0].checksum ? E('li', {}, '%s: %s'.format(_('MD5'), res[0].checksum)) : '',
-					res[0].sha256sum ? E('li', {}, '%s: %s'.format(_('SHA256'), res[0].sha256sum)) : ''
 				]));
 
 				body.push(E('p', {}, E('label', { 'class': 'btn' }, [
 					opts.keep[0], ' ', _('Keep settings and retain the current configuration')
 				])));
 
-				if (!is_valid || is_too_big)
-					body.push(E('hr'));
-
-				if (is_too_big)
-					body.push(E('p', { 'class': 'alert-message' }, [
-						_('It appears that you are trying to flash an image that does not fit into the flash memory, please verify the image file!')
-					]));
-
-				if (!is_valid)
-					body.push(E('p', { 'class': 'alert-message' }, [
-						res[2].stderr ? res[2].stderr : '',
-						res[2].stderr ? E('br') : '',
-						res[2].stderr ? E('br') : '',
-						_('The uploaded image file does not contain a supported format. Make sure that you choose the generic image format for your platform.')
-					]));
+			
+				
 
 				if (!allow_backup) {
 					body.push(E('p', { 'class': 'alert-message' }, [
@@ -257,32 +240,46 @@ return view.extend({
 							opts.skip_orig[0], ' ', _('Skip from backup files that are equal to those in /rom')
 						])));
 					}
-
 					body.push(E('p', {}, E('label', { 'class': 'btn' }, [
 						opts.backup_pkgs[0], ' ', _('Include in backup a list of current installed packages at /etc/backup/installed_packages.txt')
 					])));
 				};
 
+
+				console.log("Return ", res[1].code );
+				
 				var cntbtn = E('button', {
 					'class': 'btn cbi-button-action important',
 					'click': ui.createHandlerFn(this, 'handleSysupgradeConfirm', btn, opts),
 				}, [ _('Continue') ]);
 
-				if (res[2].code != 0) {
-					body.push(E('p', { 'class': 'alert-message danger' }, E('label', {}, [
-						_('Image check failed:'),
-						E('br'), E('br'),
-						res[2].stderr
-					])));
-				};
 
-				if ((!is_valid || is_too_big || res[2].code != 0) && is_forceable) {
-					body.push(E('p', {}, E('label', { 'class': 'btn alert-message danger' }, [
+				if (res[1].code === 2) {
+					body.push(E('pre', {}, E('label', { 'class': 'btn alert-message warning' }, [
 						opts.force[0], ' ', _('Force upgrade'),
 						E('br'), E('br'),
-						_('Select \'Force upgrade\' to flash the image even if the image format check fails. Use only if you are sure that the firmware is correct and meant for your device!')
+						res[1].stdout ? res[1].stdout : '',
+						_('The uploaded firmware have oldest version. Select \'Force upgrade\' to flash the image. Use only if you are sure that the firmware is correct!')
 					])));
 					cntbtn.disabled = true;
+					
+
+				} else if (res[1].code === 1) {
+					
+					body.push(E('pre', { 'class': 'alert-message danger' }, [
+						res[1].stdout ? res[1].stdout : '',
+						res[1].stdout ? E('br') : '',
+						_('Image check failed')
+					]));
+					cntbtn.disabled = true;
+
+				} else {
+
+
+                body.push(E('pre', { 'class': 'alert-message success' }, [
+                    _(res[1].stdout)
+                ]));
+
 				};
 
 
@@ -327,10 +324,10 @@ return view.extend({
 			if (opts[key][0].checked == opts[key][1])
 				args.push(opts[key][2]);
 
-		args.push('/tmp/firmware.bin');
+		args.push('--auto');
 
 		/* Currently the sysupgrade rpc call will not return, hence no promise handling */
-		fs.exec('/sbin/sysupgrade', args);
+		fs.exec('/sbin/update', args);
 
 		if (opts['keep'][0].checked)
 			ui.awaitReconnect(window.location.host);
@@ -438,7 +435,7 @@ return view.extend({
 
 		o = s.option(form.SectionValue, 'actions', form.NamedSection, 'actions', 'actions', _('Flash new firmware image'),
 			has_sysupgrade
-				? _('Upload a sysupgrade-compatible image here to replace the running firmware.')
+				? _('Upload a Tachyon Energy-compatible image here to replace the running firmware.')
 				: _('Sorry, there is no sysupgrade support present; a new firmware image must be flashed manually. Please refer to the wiki for device specific install instructions.'));
 
 		ss = o.subsection;
